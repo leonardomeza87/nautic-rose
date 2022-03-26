c1d63b134f12fad775c37876d6062c86697b09fe
Leonardo Meza <52134252+leonardomeza87@users.noreply.github.com>
2022-03-25 20:31:42 -0400

    Hero basic styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import "./sass/App.scss";
 import Hero from "./components/Hero";
+import Services from "./components/Services";
+import Process from "./components/Process";
+import Gallery from "./components/Gallery";
+import About from "./components/About";
+import Team from "./components/Team";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div className="App">
       <Hero />
+      <Services />
+      <Process />
+      <Gallery />
+      <About />
+      <Team />
+      <Contact />
     </div>
   );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 import { ReactComponent as LogoNauticRose } from "../images/logo.svg";
 import { ReactComponent as IconLanguage } from "../images/earth.svg";
-import { ReactComponent as ChevronDown } from "../images/chevron-down.svg";
+import { ReactComponent as IconChevron } from "../images/chevron-down.svg";
+
+import { ReactComponent as Margay } from "../images/hero-margay.svg";
+import HeroBG from "../images/hero-background.jpg";
 
 const Hero = () => {
   return (
@@ -21,18 +24,37 @@ const Hero = () => {
           </ul>
         </nav>
       </header>
-      <h1>Páginas y Diseños eficaces</h1>
-      <p>Personaliza tu marca o escoge una de nuestras mejores plantillas</p>
-      <button>Proyectos</button>
-      <button>Contáctanos</button>
-      <div className="lang-selection">
-        <button className="lang-selection-btn">
-          <IconLanguage />
-          <ChevronDown />
-        </button>
-        <div className="options">
-          <button>ESP</button>
-          <button>ENG</button>
+      <div className="content">
+        <div className="text-content">
+          <h1>Páginas y Diseños eficaces</h1>
+          <p>
+            Personaliza tu marca o escoge una de nuestras mejores plantillas
+          </p>
+          <button>Proyectos</button>
+          <button>Contáctanos</button>
+        </div>
+        <div className="decor">
+          <Margay className="margay" />
+          <div className="hero-bg">
+            <div className="inner-container">
+              {/* <img
+                src={HeroBG}
+                alt="Decoration Illustration"
+                width="100%"
+                height="100%"
+              /> */}
+            </div>
+          </div>
+        </div>
+        <div className="lang-selection">
+          <button className="lang-selection-btn">
+            <IconLanguage />
+            <IconChevron />
+          </button>
+          <div className="options">
+            <button>ESP</button>
+            <button>ENG</button>
+          </div>
         </div>
       </div>
     </section>
