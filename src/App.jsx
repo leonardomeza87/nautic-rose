33a7c9edefb3396cfd2fabc5b945f727d71383aa
Leonardo Meza <52134252+leonardomeza87@users.noreply.github.com>
2022-04-15 11:16:08 -0400

    Add comp wave

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,21 @@ import Gallery from "./components/Gallery";
 import About from "./components/About";
 import Team from "./components/Team";
 import Contact from "./components/Contact";
+import Wave from "./components/Wave";
 
 function App() {
   return (
     <div className="App">
       <Hero />
       <Services />
+      <Wave inverted={false} stamp={"vzla"} left rotation={"positive"} />
       <Process />
+      <Wave inverted={true} stamp={"wave"} rotation={"negative"} />
       <Gallery />
       <About />
+      <Wave inverted={true} stamp={"post"} left rotation={"negative"} />
       <Team />
+      <Wave inverted={false} stamp={"mail"} rotation={"positive"} />
       <Contact />
     </div>
   );
